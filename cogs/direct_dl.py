86b0df7bcd01926e678ef0332bf8b41a7ddb2dc9
Cody <vzualbandt@gmail.com>
2024-10-27 14:43:19 -0700

    hmmmst

--- cogs/direct_dl.py
+++ cogs/direct_dl.py
@@ -16,6 +16,7 @@ class DirectDLCog(commands.Cog):
         self.sio = socketio.AsyncClient()
 
         self.bot.loop.create_task(self.connect_to_socket())
+        self.call_backs()
     async def connect_to_socket(self):
         try:
             await sio.connect("http://127.0.0.1:42069")
