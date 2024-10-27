807fe14f7700dff046571ad8cdfd6e3b7c23942e
Cody <vzualbandt@gmail.com>
2024-10-27 12:48:37 -0700

    debug

--- cogs/direct_dl.py
+++ cogs/direct_dl.py
@@ -120,15 +120,18 @@ class DirectDLCog(commands.Cog):
     @sio.on('progress_update')
     async def progress_update(data):
         progress = data.get('progress')
+        print("progress")
         if progress and self.download_message:
             await self.download_message.edit(content=f"progress: {progress}")
     @sio.on('download_complete')
     async def download_complete(data):
+        print("complete")
         message = data.get('message', 'Download complete!')
         if self.download_message:
             await self.download_message.edit(content=message)
     @sio.on('download_error')
     async def download_error(data):
+        print("error")
         error_message = data.get('message', 'An error occured')
         if self.download_message:
             await self.download_message.edit(content=f"Error: {error_message}")
