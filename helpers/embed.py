ce7b17470b1de79954573b640d597cf3bcec2ad6
Cody <vzualbandt@gmail.com>
2024-11-08 03:54:05 -0800

    fixes

--- helpers/embed.py
+++ helpers/embed.py
@@ -133,7 +133,7 @@ def download_ready_from_queue(author, magnet, link=None):
 
 def download_ready(author, magnets, link=None):
     embed = Embed(description=f"<@{author}>")
-    for magnet in magnets:
+    for magnet in magnets['ready']:
         if link is None:
             link = f"{DEBRID_WEBDAV}magnets/{quote(magnet)}/"
         embed.add_field(
