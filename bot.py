de8b3efdd08b53b5a6d77c3d287d851480c76b88
Cody <vzualbandt@gmail.com>
2024-11-21 13:40:26 -0800

    duct tape fix

--- bot.py
+++ bot.py
@@ -29,10 +29,7 @@ class Waffle(commands.Bot):
         await self.process_commands(message)
 
     async def httpx_request(self, url, cookies=None):
-        if isinstance(cookies, str):
-            cookie = httpx.Cookies()
-            cookie.set((cookies.keys())[0], (cookies.values())[0], domain='app.musicleague.com')
-
+        cookie = {"app.musicleague.com":"MTczMjEzNTc0N3xMNkNTOURrY2stRTdpMVl5TW81RWFqVGlBVXJKa3RVT01aTjdjM2tWdkVtZGNPMlpiLVAxZmpKazU3bFI2RGdHcXNXaVFCSEpudWJkUldnalFXN3VaTFRoUExHSWFZUUVxSnJXUHAwTWV5RDBWQjU1Sld3X3ZJbkJvbWNlbXVCbTl4LTdwbkhLNmc1Vl9aajJ6NUJ2R3VQR2J2YzZFYnVET2E1Qk9RdzJVX2I2T29ITUk4NzZwQkdreWFhVk5OSEJGMTBwOTJXR2VUUzBLa29XcEtkTFlKdWVKUnVFR1lieW5VWGhHMDhkT1dLQjdsZFBQN25yNGFVLUdrQ19GaVZSMmNzRWJnZ0pCWjBrZ3NsTURRLWtPY0wzdVRFNDJGR3N6RDhOX0RLR09DRVB3ZjNfeDFkVnpKOFdPTnNWQXdVb25DME5kLUgtT1FLY3NPODBLYWNfamh2bWp1eV9MUUVpSEFLd3BUemJsOER0OS1rc0dKRHozaUI4YWttY0RLQUhfM3prN1RzdG9lZ1J5eHg2Ym5FZm9qTE9PdTdGZzRJdVBDQU5kXzhoZ1pJRUdieTZuWXZDd1hRZWJPdVcyaThnT0NiSTYxNlpncUJMajVjUGpOQVRyQTBiZWwyb0xBZ0FfSlYzQXh1aTQ0QlRTNTFNNnZNVnB3SVU2Tmk2VFhwZ1oyU3ZCZ2xmSjZQcjhlUU5pWGZxOU4xaDJTTEN4bjdrOUZyYkhhdzdXdlpveUNxLWt3a1YtUzhQYmNpdUJRdHFCaWVLSTQ2SkQxOGdpckY2S3lROC1ybE1QWDBqSllRMHQxUEdnQVFFenVqOWtuUktIQ2hZZVRoZUFBcUpqQWIwOHhDSWdnLVdaN0lOX3ladEx5ajV0TlhiX3V0b3ZaRExHRm9odFpuRFJXT19qS195X2g2WUJxXzVQRXFHWnNCaEF4VmdRaFJ0cXp0X1RfYzNteEkzanh2NU9XV0NLV1JQa0s4ck85R1VRR2t1ZzRLbWExT3gzZmxpekZqaGxxZ05tdDJiT1JYQ1lMSF9HUT09fF9aE4S1B8g9LqTRsKjhcwymtvZ2gI0zt943vaJAS_Ru"}
         headers = {
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:132.0) Gecko/20100101 Firefox/132.0"
         }
