d80135d6ad44f263aee4b528161de52281d288ee
cody <cody>
2025-01-19 15:47:55 -0800

    hopefully fixed the regex

--- cogs/direct_dl.py
+++ cogs/direct_dl.py
@@ -116,7 +116,7 @@ class DirectDLCog(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         tiktok_url_pattern = r"(https?://(?:www\.)?(?:vt\.)?tiktok\.com/[^\s]+)"
-        instagram_reel_regex = r"https://(www\.)?instagram\.Llcom/reel/\w+"
+        instagram_reel_regex = r"(https?://(?:www\.)?instagram\.com/reels/\w+"
         #youtube_shorts_regex = r"https://(www\.)?youtube\.com/shorts/\w+"
         combined_regex = f"({tiktok_url_pattern})|({instagram_reel_regex})"
         match = re.search(combined_regex, message.content)
