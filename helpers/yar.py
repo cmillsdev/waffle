b07e8b4ba3f597f790b593c89194b20a7d0fe148
cody <cody>
2025-01-24 12:28:28 -0800

    try-except

--- helpers/yar.py
+++ helpers/yar.py
@@ -37,23 +37,25 @@ def jackett_search(query):
     # rss.channel.item
     # get results from both, combine, filter down to handful of keys, sort by seeders, slice down to 10
     filtered_results = []
-    for i in indexers:
-        url = f"http://dietpi:9117/api/v2.0/indexers/{i}/results/torznab/api?apikey={JACKETT_KEY}&t=search&q={query}"
-        with httpx.Client() as client:
-            response = client.get(url, timeout=60)
+    try:
+        for i in indexers:
+            url = f"http://dietpi:9117/api/v2.0/indexers/{i}/results/torznab/api?apikey={JACKETT_KEY}&t=search&q={query}"
+            with httpx.Client() as client:
+                response = client.get(url, timeout=60)
+            torrent_results = xmltodict.parse(response.text)['rss']['channel']['item']
+            for torrent in torrent_results:
+                for value in torrent["torznab:attr"]:
+                    if value["@name"] == "seeders":
+                        seeders = value["@value"]
+                    if value["@name"] == "peers":
+                        peers = value["@value"]
+                item = {"name": torrent['title'], "magnet_url": torrent['guid'], "size_in_bytes": torrent["size"], "source": torrent["jackettindexer"]["#text"], "seeders": int(seeders), "leechers": peers}
+                filtered_results.append(item)
 
-        torrent_results = xmltodict.parse(response.text)['rss']['channel']['item']
-        for torrent in torrent_results:
-            for value in torrent["torznab:attr"]:
-                if value["@name"] == "seeders":
-                    seeders = value["@value"]
-                if value["@name"] == "peers":
-                    peers = value["@value"]
-            item = {"name": torrent['title'], "magnet_url": torrent['guid'], "size_in_bytes": torrent["size"], "source": torrent["jackettindexer"]["#text"], "seeders": int(seeders), "leechers": peers}
-            filtered_results.append(item)
-
-    sorted_results = sorted(filtered_results, key=lambda item: item['seeders'], reverse=True)
-    return sorted_results[:10]
+        sorted_results = sorted(filtered_results, key=lambda item: item['seeders'], reverse=True)
+        return sorted_results[:10]
+    except:
+        return 0
 
 
 def eval_pick(pick):
