448724f004903d6862659fca81c53cf4cc256ee1
Cody <email@email.com>
2025-01-26 17:36:42 -0600

    typo

--- cogs/direct_dl.py
+++ cogs/direct_dl.py
@@ -151,7 +151,7 @@ class DirectDLCog(commands.Cog):
                 )
 
                 await message.delete()
-                await message.channel.send(f"<@{message.author.id}>\n{video_info['desc']}", file=file)
+                await message.channel.send(f"<@{message.author.id}>\n>>> {video_info['desc']}", file=file)
 
                 # Step 9: Clean up the local files asynchronously
                 # there should be no mp4/webm in the folder
