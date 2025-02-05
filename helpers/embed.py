e6c5c1a677f4da31322f844429a3e8d6ec4deeda
Cody <vzualbandt@gmail.com>
2025-02-05 00:53:49 -0800

    adjustment

--- helpers/embed.py
+++ helpers/embed.py
@@ -156,7 +156,7 @@ def torrent_results(results):
         x = 0
         for torrent in results:
             result_value = f"Seeders: {torrent['seeders']} | Leechers: {torrent['leechers']} | Size: {size(torrent['size_in_bytes'])}"
-            if len(torrent['name']) > 255:
+            if len(torrent['name']) > 250:
                 name = torrent['name'][:245] + '...'
             else:
                 name = torrent['name']
